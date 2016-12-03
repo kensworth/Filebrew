a8a0ae3103783a2ef6e995dbddf784d105241fae
Ken Zhang <kyz211@nyu.edu>
2016-12-03 17:09:45 -0500

    add second ajax interaction. get retrieval working

--- app/components/Receive.js
+++ app/components/Receive.js
@@ -8,55 +8,66 @@ class Receive extends Component {
     this.props.client.on('error', function (err) {
         console.error('ERROR: ' + err.message)
     });
+    this.onTorrent = this.onTorrent.bind(this);
     if (this.props.receiving) {
-      this.props.client.add(decodeURIComponent(this.getCookie('magnet')), this.onTorrent);
+      $.ajax({
+        type: 'POST',
+        url: '/retrieve-magnet',
+        data: {
+          hash: window.location.pathname.slice(1) 
+        }
+      })
+      .done((data) => {
+        if (!data) {
+          // error handling
+          console.log('invalid link');
+        }
+        this.props.client.add(data.magnetURI, this.onTorrent);
+      });
     }
   }
   onTorrent(torrent) {
-    log('Got torrent metadata!');
-    log(
+    const self = this;
+    self.log('Got torrent metadata!');
+    self.log(
       'Torrent info hash: ' + torrent.infoHash + ' ' +
       '<a href="' + torrent.magnetURI + '" target="_blank">[Magnet URI]</a> ' +
       '<a href="' + torrent.torrentFileBlobURL + '" target="_blank" download="' + torrent.name + '.torrent">[Download .torrent]</a>'
     );
 
     // Print out progress every 5 seconds
-    var interval = setInterval(function () {
-      log('Progress: ' + (torrent.progress * 100).toFixed(1) + '%')
+    const interval = setInterval(function () {
+      self.log('Progress: ' + (torrent.progress * 100).toFixed(1) + '%')
     }, 5000);
 
     torrent.on('done', function () {
-      log('Progress: 100%');
+      self.log('Progress: 100%');
       clearInterval(interval);
     })
 
     // Render all files into to the page
     torrent.files.forEach(function (file) {
       file.appendTo('.log');
-      log('(Blob URLs only work if the file is loaded from a server. "http//localhost" works. "file://" does not.)')
       file.getBlobURL(function (err, url) {
         if (err) return log(err.message)
-        log('File done.');
-        log('<a href="' + url + '">Download full file: ' + file.name + '</a>');
+        self.log('File done.');
+        self.log('<a href="' + url + '">Download full file: ' + file.name + '</a>');
       });
     });
   }
   log(str) {
-    var p = document.createElement('p');
+    const p = document.createElement('p');
     p.innerHTML = str;
     document.querySelector('.log').appendChild(p);
   }
   getCookie(name) {
-    var value = "; " + document.cookie;
-    var parts = value.split("; " + name + "=");
+    const value = "; " + document.cookie;
+    const parts = value.split("; " + name + "=");
     if (parts.length == 2) return parts.pop().split(";").shift();
   }
   render() {
     return (
       <div className={styles.App}>
-        <div className={styles.Receive}>
-        	<p>Receive Area!</p>
-        </div>
         <div className="log"></div>
       </div>
     );
--- app/routes/functions.js
+++ app/routes/functions.js
@@ -4,6 +4,7 @@ const md5 = require('md5');
 
 const hashToMagnet = {};
 
+// create md5 digest of magnet URI, using it as the key for the URI in the hashToMagnet key-value store.
 router.post('/create-hash', (req, res) => {
 	const magnet = req.body.magnet;
 	const hash = md5(magnet);
@@ -11,6 +12,15 @@ router.post('/create-hash', (req, res) => {
         hashToMagnet[hash] = magnet;
     }
     res.send({hash});
+});
+
+router.post('/retrieve-magnet', (req, res) => {
+    const hash = req.body.hash; 
+    if (hash in hashToMagnet) {
+    	res.send({magnetURI: hashToMagnet[hash]})
+    } else {
+        res.send(false);
+    }
 });
 
 module.exports = router;

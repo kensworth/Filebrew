946300d8bfb58ca1c98e6c811e477e7ecea21c0c
Ken Zhang <kyz211@nyu.edu>
2016-12-03 22:41:36 -0500

    update logo

--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,12 @@ module.exports = {
     }, {
       test: /\.css$/,
       loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]'
+    }, {
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      loaders: [
+        'file?hash=sha512&digest=hex&name=[hash].[ext]',
+        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+      ]
     }]
   }
 };

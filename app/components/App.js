0b4602ad8acc5e3a122c6111af7c2fd18ec3403f
Ken Zhang <kyz211@nyu.edu>
2016-12-07 00:49:17 -0500

    remove import

--- app/components/App.js
+++ app/components/App.js
@@ -3,42 +3,41 @@ import WebTorrent from 'webtorrent';
 import dragDrop from 'drag-drop';
 import styles from '../styles/App.css';
 import coffee from '../../images/coffee.png';
-import swirl from '../../images/swirl.png';
 import Receive from './Receive';
 import $ from 'jquery';
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.client = new WebTorrent();
-    this.state = {
-      receiving: window.location.pathname !== '/'
-    }
-    this.createTorrent = this.createTorrent.bind(this);
-    dragDrop('body', this.createTorrent);
+constructor(props) {
+  super(props);
+  this.client = new WebTorrent();
+  this.state = {
+    receiving: window.location.pathname !== '/'
   }
-  createTorrent(files) {
-    this.client.seed(files, (torrent) => {
-      console.log('Client is seeding ' + torrent.magnetURI);
-      $.ajax({
-        type: 'POST',
-        url: '/create-hash',
-        data: {
-          magnet: torrent.magnetURI
-        }
-      })
-      .done((data) => {
-        const URI = document.createTextNode(window.location.href + data.hash);
-        document.getElementById('linkArea').appendChild(URI);
-        document.getElementById('coffee').className = styles.MovingLogo;
-      });
+  this.createTorrent = this.createTorrent.bind(this);
+  dragDrop('body', this.createTorrent);
+}
+createTorrent(files) {
+  this.client.seed(files, (torrent) => {
+    console.log('Client is seeding ' + torrent.magnetURI);
+    $.ajax({
+      type: 'POST',
+      url: '/create-hash',
+      data: {
+        magnet: torrent.magnetURI
+      }
+    })
+    .done((data) => {
+      const URI = document.createTextNode(window.location.href + data.hash);
+      document.getElementById('linkArea').appendChild(URI);
+      document.getElementById('coffee').className = styles.MovingLogo;
     });
-  }
-  render() {
-    const firstPrompt = this.state.receiving ? 'Your file is being brewed.' : 'Drop a file into the cup to start seeding.';
-    const secondPrompt = this.state.receiving ? 'Please keep your browser open until the download finishes!' : 'Copy/Paste the URL to a friend to share the file. Make sure you keep your browser open!';
-    //<div id="coffee" className={this.state.receiving ? styles.MovingLogo : styles.StaticLogo}></div>
+  });
+}
+render() {
+  const firstPrompt = this.state.receiving ? 'Your file is being brewed.' : 'Drop a file into the cup to start seeding.';
+  const secondPrompt = this.state.receiving ? 'Please keep your browser open until the download finishes!' : 'Copy/Paste the URL to a friend to share the file. Make sure you keep your browser open!';
+  //<div id="coffee" className={this.state.receiving ? styles.MovingLogo : styles.StaticLogo}></div>
     return (
       <div className={styles.App}>
         <div className={styles.AppHeader}>

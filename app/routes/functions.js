b76559e1f1c92f69703f5375cf7d53bf7ac8afb9
Ken Zhang <kyz211@nyu.edu>
2016-12-10 16:28:01 -0500

    expire keys after 24 hours

--- app/routes/functions.js
+++ app/routes/functions.js
@@ -2,22 +2,22 @@ const express = require('express');
 const env = require('dotenv').config();
 const router = express.Router();
 const md5 = require('md5');
-let client;
+let redis;
 
 if (process.env.REDISTOGO_URL) {
     const rtg = require('url').parse(process.env.REDISTOGO_URL);
-    client = require('redis').createClient(rtg.port, rtg.hostname);
-    client.auth(rtg.auth.split(":")[1]);
+    redis = require('redis').createClient(rtg.port, rtg.hostname);
+    redis.auth(rtg.auth.split(":")[1]);
 } else {
     // running filebrew locally, must run $ redis-server
-    client = require('redis').createClient();
+    redis = require('redis').createredis();
 }
 
-client.on('ready', function() {
-    console.log('Redis ready');
+redis.on('ready', function() {
+    console.log('Redis ready!');
 });
 
-client.on('error', function (err) {
+redis.on('error', function (err) {
     console.log("Error " + err);
 });
 
@@ -25,13 +25,15 @@ client.on('error', function (err) {
 router.post('/create-hash', (req, res) => {
 	const magnet = req.body.magnet;
 	const hash = md5(magnet);
-    client.set(hash, magnet);
+    redis.set(hash, magnet);
+    // expire keys after 24 hours
+    redis.expire(hash, 60 * 60 * 24);
     res.send({hash});
 });
 
 router.post('/retrieve-magnet', (req, res) => {
     const hash = req.body.hash; 
-    client.get(hash, (err, reply) => {
+    redis.get(hash, (err, reply) => {
         if (reply) {
             res.send({magnetURI: reply})
         } else {
